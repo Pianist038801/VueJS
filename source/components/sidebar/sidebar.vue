f2f6e421c3b6366dce22039b6c82952a652ac8df
Pianist038801 <maximivanov818@gmail.com>
2018-05-22 05:36:25 +0900

    add: MRN

--- source/components/sidebar/sidebar.vue
+++ source/components/sidebar/sidebar.vue
@@ -27,6 +27,9 @@
                 .sidebar__section--note Date of Birth
                 .sidebar__section--info {{ pacients[currentPacient].DateOfBirth | moment("MMM DD, YYYY")}}
             .sidebar__section
+                .sidebar__section--note MRN
+                .sidebar__section--info {{ pacients[currentPacient].MRN }}    
+            .sidebar__section
                 .sidebar__section--note SSN
                 .sidebar__section--info {{ pacients[currentPacient].SSN}}
             .sidebar__section
