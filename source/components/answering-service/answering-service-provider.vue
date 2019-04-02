e2b9def44300b0612a98c96add3fde9b61f87b7a
Pianist038801 <maximivanov818@gmail.com>
2019-04-03 01:36:18 +0400

    add provider feature

--- source/components/answering-service/answering-service-provider.vue
+++ source/components/answering-service/answering-service-provider.vue
@@ -2,54 +2,39 @@
     .appointment
         .answering-service
             .answering-caller-info
-                span Call from: 253-444-5637
+                span Call from: 253-704-2531
                 div Call To: 1-800-456-9876
-                div (OmniCare Urology)
+                div (South Shore Urology)
                 div(style="height: 30px")
-                div Call matched to: Patient
+                div Call matched to: Provider
                 div Number of Matches: 1
-                div Last Action: Urology Appointment
+                div Last Action: OnCall Cardiologist
                 div(style="flex: 1; height: 3px; background: navy; margin: 10px 0;")
                 div Details
                 div(style="height: 10px")
-                .details-content
-                    .modal__content-row
-                        .modal-appointment__title
-                            .title.mod--modal-appointment(style="margin-left: 50px") South Shore Urology
-                    .details-content-urology
-                        .urology-item
-                            div Mon - Fri 9am - 4:30 pm
-                            div Lunch: 12pm - 1:15pm
-                        .urology-item
-                            div 780 Main Street 
-                            div South Weymouth, MA 02190
-                        .urology-item
-                            div Office #: 781-331-4600
-                            div Fax #: 781-337-5095
-                            div SSH Ext: x6860
+                div Practioning Nurse at OmniCare Downtown
+                div(style="margin-top: 30px") This week:
+                div() Start: 7:00 AM End: 5:00 PM
+                div() Shift: Day  Role: RN
+                div() Call Order: 18
                 div(style="margin-top: 30px") Quick Links:
-                a(href="#3", ).ui-btn.ui-btn--skin-default.ui-btn--theme-primary.menu-button.quick-link 2 Pond Park
-                a(href="#3", ).ui-btn.ui-btn--skin-default.ui-btn--theme-primary.menu-button.quick-link Cancer Center
-                a(href="#3", ).ui-btn.ui-btn--skin-default.ui-btn--theme-primary.menu-button.quick-link Consults
-                a(href="#3", ).ui-btn.ui-btn--skin-default.ui-btn--theme-primary.menu-button.quick-link Courtesy Coach
-                a(href="#3", ).ui-btn.ui-btn--skin-default.ui-btn--theme-primary.menu-button.quick-link Emergency Department
-                a(href="#3", ).ui-btn.ui-btn--skin-default.ui-btn--theme-primary.menu-button.quick-link Engineering
-                a(href="#3", ).ui-btn.ui-btn--skin-default.ui-btn--theme-primary.menu-button.quick-link Health Info Mgmt
-                a(href="#3", ).ui-btn.ui-btn--skin-default.ui-btn--theme-primary.menu-button.quick-link Human Resources
-                a(href="#3", ).ui-btn.ui-btn--skin-default.ui-btn--theme-primary.menu-button.quick-link Supply Chain Mgmt
-                a(href="#3", ).ui-btn.ui-btn--skin-default.ui-btn--theme-primary.menu-button.quick-link Neat Line
-                a(href="#3", ).ui-btn.ui-btn--skin-default.ui-btn--theme-primary.menu-button.quick-link Patient Flow/Discharge
-                a(href="#3", ).ui-btn.ui-btn--skin-default.ui-btn--theme-primary.menu-button.quick-link Physician Referral
-                a(href="#3", ).ui-btn.ui-btn--skin-default.ui-btn--theme-primary.menu-button.quick-link.blue Other
+                a(href="#3", ).ui-btn.ui-btn--skin-default.ui-btn--theme-primary.menu-button.quick-provider-link OnCall
+                a(href="#3", ).ui-btn.ui-btn--skin-default.ui-btn--theme-primary.menu-button.quick-provider-link Department
+                a(href="#3", ).ui-btn.ui-btn--skin-default.ui-btn--theme-primary.menu-button.quick-provider-link Employee
+                a(href="#3", ).ui-btn.ui-btn--skin-default.ui-btn--theme-primary.menu-button.quick-provider-link Patient
+                a(href="#3", ).ui-btn.ui-btn--skin-default.ui-btn--theme-primary.menu-button.quick-provider-link Resource Account
+
+                div()
+                a(href="#3", ).ui-btn.ui-btn--skin-default.ui-btn--theme-primary.menu-button.quick-provider-link.blue Other
             .answering-triage-guide
-                div(style="height: 20px")
+                div(style="height: 40px")
                 span Triage Guide
                 div(style="height: 30px")
-                div The office representatives are unavailable at this time. Is this urgent or can I send a message to the office for you?
-                div(style="height: 50px")
+                div I can help you with OnCall Physician or other?
+                div(style="height: 100px")
                 .button-array
-                    a(href="#3", @click="$refs.modalbook.open()").ui-btn.ui-btn--skin-default.ui-btn--theme-primary-border.triage-button Message
-                    a(href="#3", @click="$refs.modalbook.open()").ui-btn.ui-btn--skin-default.ui-btn--theme-primary-border.triage-button Page
+                    a(href="#3", @click="$refs.modalCall.open()").ui-btn.ui-btn--skin-default.ui-btn--theme-primary-border.triage-button OnCall
+                    a(href="#3").ui-btn.ui-btn--skin-default.ui-btn--theme-primary-border.triage-button Schedule
                 .button-array
                     a(href="#3", @click="$refs.modalphone.open()").ui-btn.ui-btn--skin-default.ui-btn--theme-primary-border.triage-button Urgent
                     a(href="#3", @click="$refs.modalTransfer.open()").ui-btn.ui-btn--skin-default.ui-btn--theme-primary-border.triage-button Transfer
@@ -277,53 +262,12 @@
                     .modal-appointment__row
                             a(href="#3", @click="$refs.modalphone.close()").ui-btn.ui-btn--skin-default.ui-btn--theme-primary-border CANCEL
                             a(href="#3", @click="submit").ui-btn.ui-btn--skin-default.ui-btn--theme-primary DIAL
-            modal(ref="modalbook")
+            modal(ref="modalCall")
                 .modal__content
-                    .modal__content-row
-                        .modal__content-col
-
-                            .modal-appointment__title
-                                .title.mod--modal-appointment Send SMS
-
-                    .modal__content-row 
-                        
-                            span().inline-present Provider Name:
-                            span().inline-present(style="margin-left: 10px; margin-top: -10px; width: 30px") OmniCare Urology
-                        
-                            span().inline-present(style="margin-left: 100px") Patient Name:  Johns Jacobs
-                        
-                            span().inline-present(style="margin-left: 70px; margin-right: 20px") Template:
-                            multiselect(
-                                :options=['Contact Patient', 'Contact Agent'],
-                                :searchable="false",
-                                :allowEmpty="false",
-                                :showLabels="false",
-                                placeholder="Contact Agent",
-                                style="margin-top: -5px"
-                            ).ui-multiselect.ui-multiselect--default.inline-block
-                    .modal-appointment__templates-messages
-                        textarea.ui-textarea.ui-textarea--skin-default.ui-textarea--theme-default.mod--sms
-                            | {{sms_template}}
-                    .modal__content-row
-                        span(style="margin-right: 50px") Priority: 
-                        .ui-checkbox(style="margin-right: 50px")
-                            input#checkbox-routine(name="checkbox-routine" type="checkbox" v-model="showRoutine").ui-checkbox__input
-                            label.ui-checkbox__label(for='checkbox-routine') Routine
-                        .ui-checkbox
-                            input#checkbox-stat(name="checkbox-stat" type="checkbox" v-model="shotStat").ui-checkbox__input
-                            label.ui-checkbox__label(for='checkbox-stat') STAT
-                    .modal__content-row(style="margin-top: 36px")
-                        span(style="margin-right: 50px") Response: 
-                        .ui-checkbox(style="margin-right: 30px")
-                            input#checkbox-call(name="checkbox-call" type="checkbox" v-model="showCall").ui-checkbox__input
-                            label.ui-checkbox__label(for='checkbox-call') Please Call
-                        .ui-checkbox
-                            input#checkbox-fyi(name="checkbox-fyi" type="checkbox" v-model="shotFYI").ui-checkbox__input
-                            label.ui-checkbox__label(for='checkbox-fyi') FYI
+                    img(src="img/on-call-dlg.png", style="width: 840px")
                             
                     .modal-appointment__row
-                        a(href="#3", @click.prevent="sendSMS()").ui-btn.ui-btn--skin-default.ui-btn--theme-primary-border Send
-                        a(href="#3", @click.prevent="$refs.modalbook.close()").ui-btn.ui-btn--skin-default.ui-btn--theme-primary-border Cancel
+                        a(href="#3", @click.prevent="$refs.modalCall.close()").ui-btn.ui-btn--skin-default.ui-btn--theme-primary-border OK
 
 </template>
 <script>
